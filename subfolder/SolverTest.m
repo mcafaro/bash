c244eb0cf340a34e02a6956c2d2bd0e7752e0c95
Mark Cafaro <34887852+mcafaro@users.noreply.github.com>
2024-02-05 10:39:00 -0500

    Update SolverTest.m

--- subfolder/SolverTest.m
+++ subfolder/SolverTest.m
@@ -1,6 +1,7 @@
 classdef SolverTest < matlab.unittest.TestCase
     methods(Test)
         function realSolution(testCase)
+            pwd
             actSolution = quadraticSolver(1,-3,2);
             expSolution = [2 1];
             testCase.verifyEqual(actSolution,expSolution)
